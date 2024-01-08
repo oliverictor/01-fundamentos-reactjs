7fb4e086485ab891e8f6ba3df826e9f5245202a9
victor.oliveira <victor.oliveira@hexagon.pro>
2024-01-08 15:32:04 -0300

    add component Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 
-import './global.css'
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
-      <Header/>
-      <Post
-        author="Victor Oliveira"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo et officia voluptate sunt debitis quisquam? Cupiditate sapiente quos cum, laborum vel ex eaque soluta dolores provident non possimus, qui fugiat!"
-      />
+      <Header />
 
-      <Post
-        author="Diego Fernandes"
-        content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          <Post
+            author="Victor Oliveira"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo et officia voluptate sunt debitis quisquam? Cupiditate sapiente quos cum, laborum vel ex eaque soluta dolores provident non possimus, qui fugiat!"
+          />
+
+          <Post author="Diego Fernandes" content="Um novo post muito legal" />
+        </main>
+      </div>
     </div>
   );
 }
