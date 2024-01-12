51d2213d4bba9288cedc6e554b4dbcb39a6d1557
victor.oliveira <victor.oliveira@hexagon.pro>
2024-01-12 09:39:44 -0300

    finaliza projeto

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,10 +2,19 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
+import { useState } from "react";
 
 export function Comment({ content, deleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     deleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
   }
 
   return (
@@ -31,9 +40,9 @@ export function Comment({ content, deleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
